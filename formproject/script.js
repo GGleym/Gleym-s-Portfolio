166ac5ccc9f3d421bd71e125b6384f38de476379
GGleym <65032704+GGleym@users.noreply.github.com>
2022-12-14 22:12:08 +0300

    Add files via upload

---
+++ formproject/script.js
@@ -0,0 +1,7 @@
+document.getElementById("label-btn-2").addEventListener("click", function() {
+    alert('Чехол не нужен?')
+}, false);
+
+document.getElementById("label-btn-1").addEventListener("click", function() {
+    alert('Мотя, нет столько денег :(')
+}, false);
